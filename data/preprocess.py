f037aa35f9e2f7549a183ab94787bc6a6f488bcd
klock1125 <klock1125@gmail.com>
2025-12-17 21:17:14 -0800

    Modified preprocess.py

--- data/preprocess.py
+++ data/preprocess.py
@@ -66,7 +66,7 @@ def process_f32b0(path, has_label=True):
         label = np.array(nib_load(os.path.join(path, 'segmentation', 'seg_mask.nii.gz')), dtype='uint8', order='C')
     images = np.stack([np.array(nib_load(os.path.join(path,'skull_strip', modal + '.nii.gz')), dtype='float32', order='C') for modal in modalities], -1)  # [240,240,155]
 
-    output = path + 'data_f32b0.pkl'
+    output = os.path.join(path, 'data_f32b0.pkl')
     mask = images.sum(-1) > 0
     for k in range(4):
 
