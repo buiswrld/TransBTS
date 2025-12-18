01ca2b112f4174fca29c1d5673a3c06d2beb12eb
klock1125 <klock1125@gmail.com>
2025-12-17 23:07:13 -0800

    Modified BraTS.py

--- data/BraTS.py
+++ data/BraTS.py
@@ -25,25 +25,8 @@ import sys
 import numpy as np
 
 def pkload(fname):
-    """
-    Load a pickle file safely, handling legacy NumPy pickles that may refer to
-    old submodules like 'numpy._core'.
-    """
     with open(fname, 'rb') as f:
-        try:
-            return pickle.load(f, encoding='latin1')
-        except ModuleNotFoundError as e:
-            # Extract the missing module name
-            missing_module = str(e).split("'")[1]
-            # Map missing module to numpy
-            sys.modules[missing_module] = np
-            # Retry loading
-            return pickle.load(f, encoding='latin1')
-        except AttributeError as e:
-            # Some pickles may have moved attributes, fallback to numpy
-            return pickle.load(f, encoding='latin1')
-
-
+        return pickle.load(f)
 
 class MaxMinNormalization(object):
     def __call__(self, sample):
