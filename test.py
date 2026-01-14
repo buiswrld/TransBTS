107f7e9c6245eab47f79028ae0b398927d575814
klock1125 <klock1125@gmail.com>
2026-01-13 17:05:05 -0800

    Fixed checkpoint issue

--- test.py
+++ test.py
@@ -98,7 +98,7 @@ def main():
 
     valid_list = os.path.join(args.root, args.valid_dir, args.valid_file)
     valid_root = os.path.join(args.root, args.data_dir)
-    valid_set = BraTS(valid_list, valid_root, mode='valid')
+    valid_set = BraTS(valid_list, valid_root, mode='valid', modality_set = args.modality_set)
     print('Samples for valid = {}'.format(len(valid_set)))
 
     valid_loader = DataLoader(valid_set, batch_size=1, shuffle=False, num_workers=args.num_workers, pin_memory=True)
