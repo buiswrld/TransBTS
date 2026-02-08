f8e621f866c8fa8836d6fef1d4d6ed3b95ada4de
klock1125 <klock1125@gmail.com>
2026-02-08 13:23:15 -0800

    commented out GradCAM

--- predict.py
+++ predict.py
@@ -202,16 +202,16 @@ def validate_softmax(
             output = logit / 8.0  # mean
 
         # --- GENERATE GRAD-CAM ---
-        if grad_target:
-            # Ensure x has requires_grad=True for the backward pass
-            x.requires_grad = True
+        # if grad_target:
+        #     # Ensure x has requires_grad=True for the backward pass
+        #     x.requires_grad = True
             
-            # Generate the 3D heatmap (D, H, W)
-            cam_3d = gradcam.generate_cam(x, class_idx=grad_target)
+        #     # Generate the 3D heatmap (D, H, W)
+        #     cam_3d = gradcam.generate_cam(x, class_idx=grad_target)
             
-            # Save the result
-            name = names[i] if names else str(i)
-            np.save(os.path.join(visual, f"{name}_gcam_cls{grad_target}.npy"), cam_3d)
+        #     # Save the result
+        #     name = names[i] if names else str(i)
+        #     np.save(os.path.join(visual, f"{name}_gcam_cls{grad_target}.npy"), cam_3d)
 
         #Dice and IoU (requires labels)
         if valid_in_train:
