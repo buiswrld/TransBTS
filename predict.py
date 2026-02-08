007ab7ea3192e94f477e0dbbfbf53d84ce4fd859
klock1125 <klock1125@gmail.com>
2026-02-08 13:37:41 -0800

    Debugging gradcam

--- predict.py
+++ predict.py
@@ -135,7 +135,6 @@ def validate_softmax(
     IOU_core_list = []
     IOU_enh_list = []
 
-    gradcam = GradCAM3D(model, target_layer_name='endconv', use_cuda=True)
 
     for i, data in enumerate(valid_loader):
         print('-------------------------------------------------------------------')
@@ -202,16 +201,17 @@ def validate_softmax(
             output = logit / 8.0  # mean
 
         # --- GENERATE GRAD-CAM ---
-        # if grad_target:
-        #     # Ensure x has requires_grad=True for the backward pass
-        #     x.requires_grad = True
+        gradcam = GradCAM3D(model, target_layer_name='endconv', use_cuda=True)
+        if grad_target:
+        # Ensure x has requires_grad=True for the backward pass
+            x.requires_grad = True
             
-        #     # Generate the 3D heatmap (D, H, W)
-        #     cam_3d = gradcam.generate_cam(x, class_idx=grad_target)
+        # Generate the 3D heatmap (D, H, W)
+            cam_3d = gradcam.generate_cam(x, class_idx=grad_target)
             
-        #     # Save the result
-        #     name = names[i] if names else str(i)
-        #     np.save(os.path.join(visual, f"{name}_gcam_cls{grad_target}.npy"), cam_3d)
+        # Save the result
+            name = names[i] if names else str(i)
+            np.save(os.path.join(visual, f"{name}_gcam_cls{grad_target}.npy"), cam_3d)
 
         #Dice and IoU (requires labels)
         if valid_in_train:
