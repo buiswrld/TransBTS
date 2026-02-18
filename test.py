e69d0dc397cb077abeee60b0e99b070c01b44b2e
klock1125 <klock1125@gmail.com>
2026-02-17 22:03:52 -0800

    Fixed time bucketes

--- test.py
+++ test.py
@@ -95,7 +95,7 @@ parser.add_argument('--input_C', default=4, type=int) #Set as 1 (only one modali
 
 parser.add_argument('--version', default='1', type=str) 
 
-parser.add_argument('--time_bucket', default='', type=str, choices=['all','0','1-12','13-24','25-36','37-48','49-60','61-72','73-84','85-96','97-108','109-120','121-132','133-144','145-156','157-168','169-180','181-192','193-204','205-216','217-228','229-242','243+'])
+parser.add_argument('--time_bucket', default='', type=str, choices=['all','0','1-10','11-20','21-30','31-40','41-50','51-60','61-70','71-90','91-110','111-130','131-150','151-170','171-200','201+'])
 
 #parser.add_argument('--grad_target', default='', type=int, choices=[1, 2, 3])
 
